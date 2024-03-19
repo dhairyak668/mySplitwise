a074213a9ddb126839d4f4505d9b1640ee9de790
dhairyak668 <dhairyak668@gmail.com>
2024-03-19 05:44:19 -0500

    initial commit

---
+++ src/main/java/edu/dhairyak/mySplitwise/MySplitwiseApplication.java
@@ -0,0 +1,13 @@
+package edu.dhairyak.mySplitwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySplitwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySplitwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dhairyak/mySplitwise/MySplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dhairyak.mySplitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
